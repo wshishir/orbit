216ebfc6b327e3fbdcecafce815111a92c1d1005
Shishir <shishirtech506@gmail.com>
2025-09-14 02:30:19 +0530

    added better auth

---
+++ prisma/migrations/20250908013722_init/migration.sql
@@ -0,0 +1,58 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'ASSISTANT');
+
+-- CreateEnum
+CREATE TYPE "public"."ContentType" AS ENUM ('ARTICLE', 'REPORT', 'LINKEDIN_POST', 'TWEET', 'GENERAL');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Chat" (
+    "id" TEXT NOT NULL,
+    "title" TEXT,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Message" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL,
+    "chatId" TEXT NOT NULL,
+    "contentType" "public"."ContentType",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE INDEX "Chat_userId_idx" ON "public"."Chat"("userId");
+
+-- CreateIndex
+CREATE INDEX "Message_chatId_idx" ON "public"."Message"("chatId");
+
+-- AddForeignKey
+ALTER TABLE "public"."Chat" ADD CONSTRAINT "Chat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Message" ADD CONSTRAINT "Message_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "public"."Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
